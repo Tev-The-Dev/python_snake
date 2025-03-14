e1fa91a7c16e849c63752111bfc2c8a6afa40299
Tevyn <tevyn.payne@gmail.com>
2025-03-13 21:31:04 -0500

    moving the snake forward only for now

--- snake_game.py
+++ snake_game.py
@@ -1,33 +1,33 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
+starting_positions = [(0,0), (-20, 0), (-40, 0)]
 snake_array = []
 
-for s in range(0,3):
-    snake = Turtle()
-    snake.shape("square")
+for s in starting_positions:
+    snake = Turtle("square")
     snake.color("white")
+    snake.penup()
+    snake.goto(s)
     snake_array.append(snake)
 
-x_position = 0
-for snake in snake_array:
-    snake.goto(x_position, 0)
-    x_position-=20
 
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
 
-
-
-
-
-
-
-
-
-
+    for seg in range(len(snake_array)-1,0,-1):
+        new_x = snake_array[seg -1].xcor()
+        new_y = snake_array[seg - 1].ycor()
+        snake_array[seg].goto(new_x, new_y)
+    snake_array[0].forward(20)
 
 
 
