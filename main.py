9e1ee05a82e6e3e883620552727f575a3ada6f28
Tevyn <tevyn.payne@gmail.com>
2025-03-15 12:49:41 -0500

    creating the functions for the food class to change position when colided with the snake

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 import time
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +10,10 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+
+
+
 screen.listen()
 screen.onkey(snake.up,"Up")
 screen.onkey(snake.down,"Down")
@@ -22,7 +27,9 @@ while game_is_on:
     #Moves the snake
     snake.move()
 
-
+    #Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 
 
